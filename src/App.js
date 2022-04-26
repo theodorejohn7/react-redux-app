c4f4868d814eafd3e672ae5bc783995610347719
theodorejohn7 <theodorejohn.s@10decoders.in>
2022-04-26 15:53:50 +0530

    simple api application completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import store from "./redux-demo/store";
+import { Provider } from "react-redux";
+
+import "./App.css";
+import UserContainer from "./components/UserContainer";
+ 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+       
+      <UserContainer />
+      
+      </div>
+    </Provider>
   );
 }
 
---
+++ src/app_old.js
@@ -0,0 +1,29 @@
+// import store from "./redux-demo/store";
+// import { Provider } from "react-redux";
+
+// import "./App.css";
+// import UserContainer from "./components/UserContainer";
+// // import CakeContainer from "./components/CakeContainer";
+// // import HooksCakeContainer from "./components/HooksCakeContainer";
+// // import IceCreamContainer from "./components/iceCreamContainer";
+// // import NewCakeContainer from "./components/NewCakeContainer";
+// // import ItemContainer from "./components/ItemContainer";
+
+// function App() {
+//   return (
+//     <Provider store={store}>
+//       <div className="App">
+//         {/* <CakeContainer />
+//         <HooksCakeContainer />
+//         <IceCreamContainer />
+//         <NewCakeContainer />
+//         <ItemContainer />
+//         <ItemContainer cake /> */}
+//       <UserContainer />
+      
+//       </div>
+//     </Provider>
+//   );
+// }
+
+// export default App;
